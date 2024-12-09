b19c4545ed6db8964d8c5e176c8a638c445584e2
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-12-09 18:29:11 +0000

    Nullable DefaultMapping

--- environment/allocations.go
+++ environment/allocations.go
@@ -9,6 +9,11 @@ import (
 	"github.com/pelican-dev/wings/config"
 )
 
+type DefaultMapping struct {
+	Ip   string `json:"ip"`
+	Port int    `json:"port"`
+}
+
 // Defines the allocations available for a given server. When using the Docker environment
 // driver these correspond to mappings for the container that allow external connections.
 type Allocations struct {
@@ -20,11 +25,7 @@ type Allocations struct {
 	// Defines the default allocation that should be used for this server. This is
 	// what will be used for {SERVER_IP} and {SERVER_PORT} when modifying configuration
 	// files or the startup arguments for a server.
-	DefaultMapping struct {
-		Ip   string `json:"ip"`
-		Port int    `json:"port"`
-	} `json:"default"`
-
+	DefaultMapping *DefaultMapping `json:"default"`
 	// Mappings contains all the ports that should be assigned to a given server
 	// attached to the IP they correspond to.
 	Mappings map[string][]int `json:"mappings"`
